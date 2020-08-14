a097ab4235ff5613c289aac70e90e5a683737352
devopsdor <devopsdor@gmail.com>
2020-08-14 18:27:46 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,35 @@
+
+function getHello() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var response = this.responseText;
+            document.getElementById("test").innerHTML = response;
+        }
+    };
+    xhttp.open("GET", "http://localhost:3000/hello", true);
+    xhttp.send();
+}
+
+
+const DOG_URL = "https://dog.ceo/api/breeds/image/random";
+
+const doggos = document.querySelector(".doggos");
+
+function addNewDoggo() {
+  const promise = fetch(DOG_URL);
+  promise
+    .then(function(response) {
+      const processingPromise = response.json();
+      return processingPromise;
+    })
+    .then(function(processedResponse) {
+      const img = document.createElement("img");
+      img.src = processedResponse.message;
+      img.alt = "Cute doggo";
+      doggos.appendChild(img);
+    });
+}
+
+//document.querySelector(".add-doggo").addEventListener("click", addNewDoggo);
+
